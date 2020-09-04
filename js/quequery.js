7c124e58b1fb170efd3cd2a212f936431e8a5160
frontendrockstarng <coltcopy@gmail.com>
2020-09-04 19:28:39 +0100

    added files hiho

---
+++ js/quequery.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#chckbox1").click(function(){
+      $(this).toggleClass("mr-2-kay"); 
+ 
+  });
+
+  $("#chckbox2").click(function(){
+    $(this).toggleClass("mr-2-kay");
+  
+});
+
+$("#chckbox3").click(function(){
+  $(this).toggleClass("mr-2-kay");
+  
+});
+    
+   
+});
